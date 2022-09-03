0200e4b8b1ec94454b79f0b24b392b2326087855
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-03 15:44:49 +0000

    0 args ends in .

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -3,14 +3,17 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv
     args = len(argv) - 1
+    punc = ":"
     i = 0
 
     if args == 1:
         arg_str = "argument"
+    elif args == 0:
+        punc = "."
     else:
         arg_str = "arguments"
 
-    print("{:d} {:s}:".format(args, arg_str))
+    print("{:d} {:s}{:s}".format(args, arg_str, punc))
 
     for i in range(1, len(argv)):
         print("{:d}: {:s}".format(i, argv[i]))
