95c59cef801461a0ba2f09b4dd438bdcb3c45e99
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-03 15:40:51 +0000

    if name = main

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv
-args = len(argv) - 1
-i = 0
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    args = len(argv) - 1
+    i = 0
 
-if args == 1:
-    arg_str = "argument"
-else:
-    arg_str = "arguments"
+    if args == 1:
+        arg_str = "argument"
+    else:
+        arg_str = "arguments"
 
-print("{:d} {:s}:".format(args, arg_str))
+    print("{:d} {:s}:".format(args, arg_str))
 
-for i in range(1, len(argv)):
-    print("{:d}: {:s}".format(i, argv[i]))
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
