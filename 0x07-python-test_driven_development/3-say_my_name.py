6e1795940a5d879155901752ad4daf4399119e32
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-09 21:00:22 +0000

    task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" prints your name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ check for str, then print """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is " + first_name + " " + last_name)
