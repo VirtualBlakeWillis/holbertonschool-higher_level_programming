ee6c7d4d8e9f11071a76024b4f6508eb388fcc5d
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-09 18:28:05 +0000

    changed rerror messages

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,18 +8,12 @@ def add_integer(a, b=98):
     """ math and type checking """
 
 
-    n1 = a
-    n2 = b
-    if type(a) is float:
-        n1 = int(a)
-    if type(b) is float:
-        n2 = int(b)
+    n1 = int(a)
+    n2 = int(b)
 
     if type(n1) is not int:
         raise TypeError("a must be an integer")
     if type(n2) is not int:
         raise TypeError("b must be an integer")
 
-    if a+1 == a:
-        raise OverflowError("a too large")
     return (n1 + n2)
