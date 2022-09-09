4d12e9a4e4d51a34ae480ebfa85f7b23b64f687c
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-09 18:42:38 +0000

    changed error messages, and when i check type

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,13 +6,14 @@ return a + b
 
 def add_integer(a, b=98):
     """ math and type checking """
-
+    type_a = type(a)
+    type_b = type(b)
+    if type_a is not int and type_a is not float:
+        raise TypeError("a must be an integer")
+    
+    if type_b is not int and type_b is not float:
+        TypeError("b must be an integer")
+    
     n1 = int(a)
     n2 = int(b)
-
-    if type(n1) is not int:
-        raise TypeError("a must be an integer")
-    if type(n2) is not int:
-        raise TypeError("b must be an integer")
-
     return (n1 + n2)
