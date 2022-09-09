3cc7d4e87d1f054a0b656ba81a3989d970374770
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-09 19:09:26 +0000

    first check

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" divide a matrix by div"""
+
+
+def matrix_divided(matrix, div):
+    """ much error checking, then maths"""
+
+    for row in matrix:
+        for val in row:
+            val_type = type(val)
+            if val_type is not int and val_type is not float:
+                raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
+
+    matrix_row_size = len(matrix[0])
+    for row in matrix:
+        if len(row) != matrix_row_size:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    div_type = type(div)
+    if div_type is not int and div_type is not float:
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = [round(val/div, 2) for row in matrix for val in row]
+    return new_matrix
