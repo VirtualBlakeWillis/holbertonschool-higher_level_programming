d92abc17665431af67cf8e937258491dd364b067
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-09 17:29:11 +0000

    skeleton of task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """ return a + b """
+    n1 = a
+    n2 = b
+    if type(a) is float:
+        n1 = int(a)
+    if type(b) is float:
+        n2 = int(b)
+
+    if type(n1) is not int:
+        raise TypeError("a must be an integer")
+    if type(n2) is not int:
+        raise TypeError("b must be an integer")
+    
+    return (n1 + n2)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+## Holberton School Project 0x05. Python - Test-driven development
+
+### Project information: [Click Me!](https://intranet.hbtn.io/projects/1151)
