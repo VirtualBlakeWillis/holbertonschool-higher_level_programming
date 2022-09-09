494cbffb4847ff89a05091704de2e7678b687d04
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-09 18:43:44 +0000

    pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,10 +10,10 @@ def add_integer(a, b=98):
     type_b = type(b)
     if type_a is not int and type_a is not float:
         raise TypeError("a must be an integer")
-    
+
     if type_b is not int and type_b is not float:
         TypeError("b must be an integer")
-    
+
     n1 = int(a)
     n2 = int(b)
     return (n1 + n2)
