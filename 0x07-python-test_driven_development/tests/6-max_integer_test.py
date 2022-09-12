3be5b48613191974bdc5d1110ea220eb38c4e102
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-12 17:05:44 +0000

    pass str in

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,14 +6,18 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+
     def test_1_num(self):
         self.assertEqual(max_integer([1]), 1)
 
     def test_no_args(self):
         self.assertEqual(max_integer(), None)
 
-    def test_strList(self):
-        self.assertRaises(TypeError, max_integer(["hi", "hello", "bye"]))
-    
     def test_floatList(self):
         self.assertEqual(max_integer([1.1, 2.2, 3.3]), 3.3)
+
+    def test_strList(self):
+        self.assertRaises(TypeError, max_integer(["hi", "hello", "bye"]))
+
+    def test_str(self):
+        self.assertRaises(TypeError, max_integer("Hello!"))
