e4b5bc258c5de34a2d7d8397834b59d6d51583b1
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-12 16:24:19 +0000

    pep8

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+""" print 2 newlines if char is in char_list """
+
+
 def text_indentation(text):
+    """ check error, then loop """
 
     char_list = [".", "?", ":"]
     if type(text) is not str:
         raise TypeError("text must be a string")
-    
+
     for char in text:
         print(char, end="")
         for comp in char_list:
