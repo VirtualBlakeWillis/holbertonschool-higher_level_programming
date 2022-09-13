efef374a82fa8d280a3b51263b684bca27b99ade
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-13 17:10:54 +0000

    0 case

--- 0x06-python-more_classes/2-rectangle.py
+++ 0x06-python-more_classes/2-rectangle.py
@@ -44,6 +44,8 @@ class Rectangle:
         return (self.__width * 2) + (self.__height * 2)
     
     def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
         rect = (("#" * self.__width) + "\n") * self.__height
         return rect
     
