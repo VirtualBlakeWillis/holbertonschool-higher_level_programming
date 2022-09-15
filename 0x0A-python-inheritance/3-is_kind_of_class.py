49075ece01842aa14ceb936dc691839341b3b3fc
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-15 19:41:58 +0000

    isinstance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" check if is kind of class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ IS A kind of class """
+
+    if isinstance(obj) is a_class:
+        return True
+    
+    return False
