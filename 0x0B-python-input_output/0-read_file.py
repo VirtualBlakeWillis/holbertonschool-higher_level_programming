3d4a713b196f785010e31243ea33ec53c87ac573
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-20 02:30:51 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@
 def read_file(filename=""):
     """ logic
     """
-    with open("filename", "r", encoding="utf=8") as f:
+    with open(filename, "r", encoding="utf=8") as f:
         for line in f:
             print(line, end='')
