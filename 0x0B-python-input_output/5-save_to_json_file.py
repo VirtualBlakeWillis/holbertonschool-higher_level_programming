b6f020bc354feac492aadcc3645254d4809d7598
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-20 03:03:04 +0000

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" save obj to json """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ logic """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, filename)
