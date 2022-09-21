3a734240b1fa0d773942cf13dfc6616d1288b386
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-21 23:03:33 +0000

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,13 @@ class Square(Rectangle):
         rep = "[Square] ({}) {}/{}".format(self.id, self.x, self.y)
         rep += " - {}".format(self.width)
         return rep
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.int_validator(value, "width")
+        self.width = value
+        self.height = value
