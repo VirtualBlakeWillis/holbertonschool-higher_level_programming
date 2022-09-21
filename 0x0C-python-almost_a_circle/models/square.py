9ab7cb17c77fdfbc64f189c6a313490fa8673d5c
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-21 22:57:47 +0000

    task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Square module that defines the Square class """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        rep = "[Square] {} {}/{}".format(self.id, self.x, self.y)
+        rep += " - {}".format(self.width)
+        return rep
