c454648a9c16b7c3a23b3007a604a70952c9ce1f
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-23 15:33:11 +0000

    doc

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,6 +32,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of a list of dictionaries"""
+
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
@@ -40,8 +41,7 @@ class Base:
     def save_to_file(cls, list_objs):
         """ Writes JSON string representation of list_objs to a file """
         my_list = []
-        fn = cls.__name__ + ".json"
         for obj in list_objs:
             my_list.append(obj.to_dictionary())
-        with open(fn, "w", encoding="utf-8") as f:
+        with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(my_list))
