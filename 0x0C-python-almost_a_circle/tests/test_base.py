51a28cdbc4f39d9dc7d8a08ee36dcc6ffcff3e2f
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-23 23:49:45 +0000

    id is str

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -15,5 +15,9 @@ class TestBase(unittest.TestCase):
         b2 = Base()
         self.assertEqual(b2.id, 1)
 
+    def test_id_string(self):
+        b3 = Base("hi")
+        self.assertEqual(b3.id, "hi")
+
 if __name__ == "__main__":
     unittest.main()
