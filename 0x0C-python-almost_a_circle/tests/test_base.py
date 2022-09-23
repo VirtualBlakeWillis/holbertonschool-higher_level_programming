ea2152c8a377e653b3b5a68484a7e4072124489f
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-23 23:31:26 +0000

    base, no id given

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -7,17 +7,9 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     """ Test Base! """
 
-    def test_id_given(self):
-        b1 = Base(10)
-        self.assertEqual(b1.id, 10)
-
     def test_id_empty(self):
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-
-    def test_id_None(self):
-        b3 = Base(None)
-        self.assertEqual(b3.id, 1)
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
 
 if __name__ == "__main__":
     unittest.main()
