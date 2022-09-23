22c70eebc84aa2c442e69e9a5871e54971b6ee75
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-23 23:17:16 +0000

    test case 1 for base

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """ Testing module for Base Class """
 import unittest
-from module.base import Base
+from models.base import Base
+
 
 class TestBase(unittest.TestCase):
-    pass
+    """ Test Base! """
+
+    def test_id(self):
+        b1 = Base(10)
+        self.assertEqual(b1.id, 10)
+
+if __name__ == "__main__":
+    unittest.main()
