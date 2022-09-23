79fcd86c5d701b1b8c43ccccab1f63ae13a5455c
VirtualBlakeWillis <bewillis101@gmail.com>
2022-09-23 23:29:54 +0000

    weird test cases

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -7,9 +7,17 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     """ Test Base! """
 
-    def test_id(self):
+    def test_id_given(self):
         b1 = Base(10)
         self.assertEqual(b1.id, 10)
+
+    def test_id_empty(self):
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+    def test_id_None(self):
+        b3 = Base(None)
+        self.assertEqual(b3.id, 1)
 
 if __name__ == "__main__":
     unittest.main()
