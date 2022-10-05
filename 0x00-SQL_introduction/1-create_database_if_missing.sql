559eae70c5072a60175bba3026c58acb16a06df9
VirtualBlakeWillis <bewillis101@gmail.com>
2022-10-05 11:45:15 -0700

    comments

--- 0x00-SQL_introduction/1-create_database_if_missing.sql
+++ 0x00-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
 -- create database hbtn_0c_0 in mySQL server
+-- create database hbtn_0d_0 in mySQL server if not already
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x00-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+- delete database hbtn_0c_0d_0 if it exists
+DELETE DATABASE IF EXISTS hbtn_0c_0;
