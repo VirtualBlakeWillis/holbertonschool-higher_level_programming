3ffc0052d6086dc1d44068ba0942342745436421
VirtualBlakeWillis <bewillis101@gmail.com>
2022-10-18 13:26:23 -0700

    BINARY

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' \
+    query = "SELECT * FROM states WHERE BINARY name = '{}' \
              ORDER BY id ASC".format(argv[4])
     cur.execute(query)
     rows = cur.fetchall()
