cac6a5f1f75ca4bad9b11371c89599ca06524c20
VirtualBlakeWillis <bewillis101@gmail.com>
2022-10-18 13:00:40 -0700

    task 1

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """ code for script """
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name like 'N%'")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print("{}".format(row))
