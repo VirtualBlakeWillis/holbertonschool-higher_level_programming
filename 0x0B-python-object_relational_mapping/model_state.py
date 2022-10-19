4fac7f9444c375ec3c227670e5f55f33c6a43227
VirtualBlakeWillis <bewillis101@gmail.com>
2022-10-19 14:08:48 -0700

    pep8 + documentation

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
+""" create table states using SQLAlchemy """
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, String, Integer
+
 Base = declarative_base()
-"""
-class State
-id int
-name str
-"""
 
 
 class State(Base):
+    """ State class """
     __tablename__ = "states"
     id = Column(Integer(), autoincrement=True, primary_key=True,
                 nullable=False)
