e26e62c98f5838bd1ef47d7100984ab76d3cc0c7
VirtualBlakeWillis <bewillis101@gmail.com>
2022-10-20 10:52:00 -0700

    task 14 test push

---
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" create table states using SQLAlchemy """
+from sqlalchemy import Column, String, Integer
+from model_state import Base
+from sqlalchemy import ForeignKey
+
+
+
+class City(Base):
+    """ State class """
+    __tablename__ = "cites"
+    id = Column(Integer(), autoincrement=True, primary_key=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"))
