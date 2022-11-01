b493f76a46ca66e8f5f0ce0cc1885d777d091629
VirtualBlakeWillis <bewillis101@gmail.com>
2022-11-01 21:43:46 +0000

    task 9, requests.auth

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -8,11 +8,13 @@ password) and uses the GitHub API to display your id
 
 if __name__ == "__main__":
     import requests
+    from requests.auth import HTTPBasicAuth
     from sys import argv
 
-    u_t = "{}:{}".format(argv[1], argv[2])
-    url = "https://api.github.com/users/{}".format(u_t)
-    req = requests.get(url).json()
+    username = argv[1]
+    password = argv[2]
+    url = "https://api.github.com/user"
+    req = requests.get(url, auth=HTTPBasicAuth(username, password)).json()
     if 'id' in req.keys():
         print(req['id'])
     else:
