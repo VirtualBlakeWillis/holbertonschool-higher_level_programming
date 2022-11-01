4ed859a12beac2d6717f5840df406790d4d289df
VirtualBlakeWillis <bewillis101@gmail.com>
2022-11-01 21:02:50 +0000

    raised status_code threshold > 200 to >=400

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     from sys import argv
     req = requests.get(argv[1])
 
-    if req.status_code > 200:
+    if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
     else:
-        print(req)
+        print(req.text)
