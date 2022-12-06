f49df91e8bc60658a9ef7c7e629f1ea3933c42c9
VirtualBlakeWillis <bewillis101@gmail.com>
2022-12-06 21:15:37 +0000

    task 6

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -6,7 +6,7 @@ const output = {};
 request.get(url, 'utf8', (error, response, body) => {
   if (error) throw error;
   const jsonData = JSON.parse(body);
-  for (item of jsonData) {
+  for (const item of jsonData) {
     if (item.completed) {
       if (output[item.userId]) {
         output[item.userId] = output[item.userId] + 1;
