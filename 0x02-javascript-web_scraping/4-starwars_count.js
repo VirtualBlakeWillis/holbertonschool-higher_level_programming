ac317a457af1e141a20534c6722768e73db0d627
VirtualBlakeWillis <bewillis101@gmail.com>
2022-12-06 20:24:02 +0000

    task 4

---
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+const wedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
+let count = 0;
+
+request.get(url, 'utf8', (error, response, body) => {
+  if (error) console.log(error);
+  else {
+    const jsonData = JSON.parse(body);
+    for (const i in jsonData.results) {
+      if (jsonData.results[i].characters.indexOf(wedgeAntilles) > -1) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
