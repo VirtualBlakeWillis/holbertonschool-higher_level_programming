c6b4ef9e6724c110a603d38d7822e368aa8e4e2b
VirtualBlakeWillis <bewillis101@gmail.com>
2022-12-06 21:15:07 +0000

    task 6

---
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+const output = {};
+request.get(url, 'utf8', (error, response, body) => {
+  if (error) throw error;
+  const jsonData = JSON.parse(body);
+  for (item of jsonData) {
+    if (item.completed) {
+      if (output[item.userId]) {
+        output[item.userId] = output[item.userId] + 1;
+      } else {
+        output[item.userId] = 1;
+      }
+    }
+  }
+  console.log(output);
+});
