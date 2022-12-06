acff248a205bfc5ea29a279024666e9a1b71fa5a
VirtualBlakeWillis <bewillis101@gmail.com>
2022-12-06 18:58:36 +0000

    attempt 2, using JSON.stringify

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const {readFileSync, promises: fsPromises} = require('fs');
+const { readFileSync } = require('fs');
 
 try {
-  const contents = readFileSync(process.argv[2], 'utf-8', );
+  const contents = readFileSync(process.argv[2], 'utf-8');
   console.log(contents);
 } catch (err) {
-  console.log(err);
+  console.log(JSON.stringify(err));
 }
