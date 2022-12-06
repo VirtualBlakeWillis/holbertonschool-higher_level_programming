e06fb55bf769d64ec251a2c4f3199c829ad3c3e6
VirtualBlakeWillis <bewillis101@gmail.com>
2022-12-06 20:36:04 +0000

    task 4, removed else statement

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -2,17 +2,16 @@
 const request = require('request');
 
 const url = process.argv[2];
+const wedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
 let count = 0;
 
 request.get(url, 'utf8', (error, response, body) => {
   if (error) throw error;
-  else {
-    const jsonData = JSON.parse(body);
-    for (const i in jsonData.results) {
-      if (jsonData.results[i].characters.indexOf('/18/') > -1) {
-        count++;
-      }
+  const jsonData = JSON.parse(body);
+  for (const i in jsonData.results) {
+    if (jsonData.results[i].characters.indexOf(wedgeAntilles) > -1) {
+      count++;
     }
-    console.log(count);
   }
+  console.log(count);
 });
